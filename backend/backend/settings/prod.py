8cb548d62e7e8e95fc51044bb7ffa2584277b842
ctivir <ctivir@gmail.com>
2021-08-21 01:36:30 +0200

    Configuring the app

--- backend/backend/settings/prod.py
+++ backend/backend/settings/prod.py
@@ -27,7 +27,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = config('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG =  config('DEBUG', default=False, cast=bool)
+DEBUG = config('DEBUG', default=False, cast=bool)
 
 ALLOWED_HOSTS = ['*']
 
@@ -127,7 +127,7 @@ USE_TZ = True
 
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
-STATIC_URL = os.path.join(BASE_DIR, 'static')
+STATIC_URL = 'static'
 
 MEDIA_URL = '/media/'
 
