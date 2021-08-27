77877ad0fc2b0a308940768033c992579cc77834
0limpi0 <olymangue@gmail.com>
2021-08-27 23:29:29 +0200

    add and style contact page

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,10 +1,11 @@
 import Home from './pages/Home';
 import './styles/global.css'
+import Contact from './pages/Contact';
 
 function App() {
   return (
     <div>
-      <Home />
+      <Contact />
     </div>
   );
 }
